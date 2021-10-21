869312de65838f517221873dac778f3fa55372fd
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2021-10-21 12:44:23 +0200

    Initial project

---
+++ src/main/java/es/pmkirsten/raceControl/Garage.java
@@ -0,0 +1,10 @@
+package es.pmkirsten.raceControl;
+
+public class Garage {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/main/java/es/pmkirsten/raceControl/ScoreCar.java
@@ -0,0 +1,9 @@
+package es.pmkirsten.raceControl;
+
+public class ScoreCar {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
